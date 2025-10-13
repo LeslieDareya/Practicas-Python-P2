bcda9bd50e6aa8c9fcae1c686c69f579b933effc
LeslieDareya <leslie90da3478@gmail.com>
2025-10-13 08:36:27 -0600

    practicas p2

--- validarRepaso.py
+++ validarRepaso.py
@@ -1,11 +1,15 @@
 class Validar():
     def __init__(self):
-        pass
+        self.index
     def ValidarAscii(self, valor):
         con = 0
+        con2 = 0
         for i in valor:
             if ord(i) >=48 and ord(i)<= 57:
                 con += 1
+            if (ord(i)>= 65 and ord()<=90) or (ord(i)>= 97 and ord()<=122):
+                con2 += 1
+                
             if con == len(valor):
                 return "Numeros"
             else:
@@ -13,6 +17,23 @@ class Validar():
 
        
     def ValidarConError(self, valor):
-        pass
+        a = 0
+        b = 0.0
+        try:
+            a = int(valor)
+            return "numeros"
+        except ValueError:
+        try:
+            b = float(valor)
+            return "Es un numero real o con decimales"
+            return "letras o numeros"
+        
+       
     def ValidarConString(self, valor):
-        pass
+       if valor[self.index] == "@":
+           return "si es un correo"
+       else:
+           if self.index < len(valor):
+           self.index += 1
+           self.ValidarConString
+           
