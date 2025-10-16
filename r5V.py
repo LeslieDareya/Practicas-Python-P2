a0944c8808ebca9b731c3480734aa585e4dd5308
LeslieDareya <leslie90da3478@gmail.com>
2025-10-15 20:52:26 -0600

    Practicas Parcial 2

--- r5V.py
+++
@@ -1,9 +0,0 @@
-class Validar():
-    def __init__(self):
-        pass
-
-    def ValidarNumero(self, valor):
-        if not valor.isdigit():
-            return "No es un número válido"
-        numero = int(valor)
-        return numero % 2 == 0
